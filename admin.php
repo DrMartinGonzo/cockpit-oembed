5481d247aeceba9d9f14c0cc64e3e64b28db852f
Martin P <miloo_de_tours@hotmail.fr>
2019-02-22 02:05:17 +0100

    update addAssets paths

--- admin.php
+++ admin.php
@@ -2,7 +2,7 @@
 
 $app->on('admin.init', function () use ($app) {
     $this->helper('admin')->addAssets([
-      'field-oembed:assets/components/field-oembed.tag',
-      'field-oembed:assets/js/oembed-renderer.js'
+      'field-oembed:assets/field-oembed.tag',
+      'field-oembed:assets/oembed-renderer.js'
       ]);
 });
